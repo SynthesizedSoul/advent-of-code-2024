50f750e70a123bc05ba3fa339791006b04680ec9
SynthesizedSoul <SynthesizedSoul@users.noreply.github.com>
2024-12-02 21:16:48 -0800

    Add Day 3 Part 2.

--- 2024-12-03/index.js
+++ 2024-12-03/index.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
 
-const regex = /mul\(([0-9]+),([0-9]+)\)/g;
+const regex = /(?:mul\(([0-9]+),([0-9]+)\)|do\(\)|don't\(\))/g;
 const input = fs.readFileSync('input', { encoding: 'utf8' });
 const found = [...input.matchAll(regex)];
-const sum = found.reduce((sum, mul) => {
-  return sum += parseInt(mul[1], 10) * parseInt(mul[2], 10);
+
+let mulEnabled = true;
+const sum = found.reduce((sum, instruction) => {
+  switch (instruction[0]) {
+    case 'do()':
+      mulEnabled = true;
+      return sum;
+    case 'don\'t()':
+      mulEnabled = false;
+      return sum;
+    default:
+      return mulEnabled ? sum += parseInt(instruction[1], 10) * parseInt(instruction[2], 10) : sum;
+  }
 }, 0);
 
 console.log(sum);
