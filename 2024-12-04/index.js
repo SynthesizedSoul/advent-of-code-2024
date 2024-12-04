7cda635f9fa17fceadcf82b7225eb7cbc41e4721
SynthesizedSoul <SynthesizedSoul@users.noreply.github.com>
2024-12-03 21:51:37 -0800

    Add Day 4 Part 2.

--- 2024-12-04/index.js
+++ 2024-12-04/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const searchWord = 'XMAS';
+const searchWord = 'MAS';
 const searchWordReversed = searchWord.split('').reverse().join('');
 
 const input = fs.readFileSync('input', { encoding: 'utf8' });
@@ -15,7 +15,7 @@ const puzzle = lines.reduce((m, line, i) => {
   return m;
 }, Array(lines.length));
 
-function checkDirection(puzzle, i, j, direction) {
+function checkDirection( i, j, direction) {
   const word = Array(searchWord.length).fill(0).reduce((word, _, index) => {
     return word += puzzle[i + (direction[0] * index)][j + (direction[1] * index)];
   }, '');
@@ -28,21 +28,9 @@ let occurences = 0;
 for (let i = 0; i < puzzle.length; i++) {
   for (let j = 0; j < puzzle[i].length; j++) {
     if (j <= puzzle[i].length - searchWord.length) {
-      if (checkDirection(puzzle, i, j, [0, 1])) occurences++;
-
       if (i <= puzzle.length - searchWord.length) {
-        if (checkDirection(puzzle, i, j, [1, 1])) occurences++;
+        if (checkDirection(i, j, [1, 1]) && checkDirection(i, j + searchWord.length - 1, [1, -1])) occurences++;
       }
-    }
-  
-    if (j >= searchWord.length - 1) {
-      if (i <= puzzle.length - searchWord.length) {
-        if (checkDirection(puzzle, i, j, [1, -1])) occurences++;
-      }
-    }
-
-    if (i <= puzzle.length - searchWord.length) {
-      if (checkDirection(puzzle, i, j, [1, 0])) occurences++;
     }
   }
 }
