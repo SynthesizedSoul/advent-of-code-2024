f3d000c2ac9af2b39c48f0965cccf20f66126a1b
SynthesizedSoul <SynthesizedSoul@users.noreply.github.com>
2024-12-04 19:46:38 -0800

    Further optimization of Day 4 Part 2.

--- 2024-12-04/index.js
+++ 2024-12-04/index.js
@@ -1,17 +1,20 @@
 const fs = require('fs');
 
+const input = fs.readFileSync('input', { encoding: 'utf8' });
+
+const start = performance.now();
+const inputLength = input.length;
 const searchWord = 'MAS';
 const searchWordReversed = searchWord.split('').reverse().join('');
-
-const input = fs.readFileSync('input', { encoding: 'utf8' });
-const lines = input.split('\n');
+const searchWordLength = searchWord.length;
+const lineLength = input.indexOf('\n') + 1;
 
 function checkDirection(i, j, direction) {
   let word = '';
   let reversed = false;
 
-  for (let k = 0; k < searchWord.length; k++) {
-    word += lines[i + (direction[0] * k)][j + (direction[1] * k)];
+  for (let k = 0; k < searchWordLength; k++) {
+    word += input[((i + (direction[0] * k)) * lineLength) + (j + (direction[1] * k))];
 
     if (k === 0 && searchWordReversed[k] === word[k]) {
       reversed = true;
@@ -25,12 +28,28 @@ function checkDirection(i, j, direction) {
 
 let occurences = 0;
 
-for (let i = 0; i <= lines.length - searchWord.length; i++) {
-  for (let j = 0; j <= lines[i].length - searchWord.length; j++) {
-    if (!checkDirection(i, j, [1, 1]) || !checkDirection(i, j + searchWord.length - 1, [1, -1])) continue;
+let i = 0;
+let j = 0;
 
-    occurences++;
+while (true) {
+  const index = (i * lineLength) + j;
+
+  // Exit out of the loop
+  if (index > inputLength - 1) break;
+
+  // We've found a newline, lets go to the next
+  if (j !== 0 && index % lineLength === 0) {
+    i++;
+    j = 0;
+    continue;
   }
+
+  if (checkDirection(i, j, [1, 1]) && checkDirection(i, j + searchWordLength - 1, [1, -1])) occurences++;
+
+  j++;
 }
 
+const end = performance.now();
+
 console.log(occurences);
+console.log(`Execution time: ${end - start} ms`);
