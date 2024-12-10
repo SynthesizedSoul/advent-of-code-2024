f5b46712404d31a9db91f027eccea242b9f17244
SynthesizedSoul <SynthesizedSoul@users.noreply.github.com>
2024-12-09 23:20:56 -0800

    Add Day 9 Part 2.

--- 2024-12-09/index.js
+++ 2024-12-09/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 
-const diskMap = fs.readFileSync('input', { encoding: 'utf8' });
-const diskVisualization = diskMap.split('').reduce((s, c, index) => {
+const diskMap = fs.readFileSync('input', { encoding: 'utf8' }).split('');
+
+const diskVisualization = diskMap.reduce((s, c, index) => {
   for (let i = 0; i < +c; i++) {
     s.push(index % 2 === 0 ? (index / 2).toString() : '.');
   }
@@ -14,12 +15,44 @@ for (let i = diskVisualization.length - 1; i >= 0; i--) {
 
   if (c === '.') continue;
 
-  const freeSpace = diskVisualization.indexOf('.');
+  const endBlock = i;
+  let startBlock = endBlock;
+  let tmpBlockIndex = endBlock;
 
-  if (freeSpace > i) break;
+  while (diskVisualization[tmpBlockIndex] === c && tmpBlockIndex >= 0) {
+    startBlock = tmpBlockIndex;
+    tmpBlockIndex--;
+  }
 
-  diskVisualization[freeSpace] = c;
-  diskVisualization[i] = '.';
+  let startFrom = 0;
+
+  while (true) {
+    let startFreeSpace = diskVisualization.indexOf('.', startFrom);
+
+    if (startFreeSpace === -1 || startFreeSpace > i) break;
+
+    let endFreeSpace = startFreeSpace;
+    let tmpFreeSpaceIndex = startFreeSpace;
+  
+    while (diskVisualization[tmpFreeSpaceIndex] === '.' && tmpFreeSpaceIndex < i) {
+      endFreeSpace = tmpFreeSpaceIndex;
+      tmpFreeSpaceIndex++;
+    }
+  
+    const blockSize = (endBlock - startBlock) + 1;
+    const freeSpace = (endFreeSpace - startFreeSpace) + 1;
+
+    if (freeSpace < blockSize) {
+      startFrom = endFreeSpace + 1;
+      continue;
+    }
+  
+    diskVisualization.splice(startFreeSpace, blockSize, ...diskVisualization.splice(startBlock, blockSize, ...Array(blockSize).fill('.')));
+
+    break;
+  }
+
+  i = startBlock; // We need to move the index this many
 }
 
 const diskChecksum = diskVisualization.reduce((checksum, c, index) => {
