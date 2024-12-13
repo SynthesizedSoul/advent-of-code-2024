59775d0f07f99cde727549151b0a5062e3512816
SynthesizedSoul <SynthesizedSoul@users.noreply.github.com>
2024-12-13 01:13:48 -0800

    Add Day 13 Part 2.

--- 2024-12-13/index.js
+++ 2024-12-13/index.js
@@ -7,81 +7,46 @@ class Button {
     this.yDistance = yDistance;
     this.cost = cost;
   }
-
-  costPerDistanceUnit() {
-    return 1.0 * (this.xDistance + this.yDistance) / this.cost;
-  }
 }
 
 class Prize {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
+    this.x = x + 10000000000000;
+    this.y = y + 10000000000000;
   }
 }
 
 class Machine {
-  constructor(buttonA, buttonB, prize, maxTokens = 100) {
+  constructor(buttonA, buttonB, prize) {
     this.x = 0;
     this.y = 0;
-    this.pressedButtons = [];
     this.tokens = 0;
-    this.maxTokens = 100;
 
     this.buttonA = buttonA
     this.buttonB = buttonB;
     this.prize = prize;
   }
 
-  reverse(steps = 1) {
-    for (let i = 0; i < steps; i++) {
-      const [unpressButton] = this.pressedButtons.splice(this.pressedButtons.length - 1, 1);
-
-      this.x -= unpressButton.xDistance;
-      this.y -= unpressButton.yDistance;
-      this.tokens -= unpressButton.cost;
-    }
-  }
-
-  pressButton(name) {
+  pressButton(name, times = 1) {
     let button = name === 'A' ? this.buttonA : this.buttonB;
 
-    this.x += button.xDistance;
-    this.y += button.yDistance;
-    this.tokens += button.cost;
-    this.pressedButtons.push(button);
+    this.x += button.xDistance * times;
+    this.y += button.yDistance * times;
+    this.tokens += button.cost * times;
   }
 
   isAtPrize() {
     return this.x === this.prize.x && this.y === this.prize.y;
   }
 
-  isFurtherThanPrize() {
-    // This currently requires buttons to never move negative spaces
-    return this.x > this.prize.x || this.y > this.prize.y;
-  }
-
   attemptMoveToPrize() {
-    // a*x1 + b*x2 = x
-    // a*y1 + b*y2 = y
-    // a = (y - b*y2)/y1
-    // (y - b*y2)*x1/y1 + b*x2 = x
-    // y*x1/y1 - b*y2*x1/y1 + b*x2 = x
-    // x - y*x1/y1 = b*x2 - b*y2*x1/y1
-    // (x - y*x1/y1)/(x2 - y2*x1/y1) = b
-
     const bPresses = Math.round((this.prize.x - this.prize.y * this.buttonA.xDistance / this.buttonA.yDistance) / (this.buttonB.xDistance - this.buttonB.yDistance * this.buttonA.xDistance / this.buttonA.yDistance));
     const aPresses = (this.prize.x - bPresses * this.buttonB.xDistance) / this.buttonA.xDistance;
 
     if (aPresses % 1 != 0 || bPresses % 1 != 0) return;
 
-    for (let i = 0; i < aPresses; i++) {
-      this.pressButton('A');
-    }
-
-    for (let i = 0; i < bPresses; i++) {
-      this.pressButton('B');
-    }
+    this.pressButton('A', aPresses);
+    this.pressButton('B', bPresses);
   }
 }
 
